21bad0826e98f620f529168c25ea989cd7c78539
Shu <shunobi@gmail.com>
2012-07-05 22:46:38 -0700

    Initial Commit

---
+++ traderpog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  traderpog
+//
+//  Created by Shu Chiun Cheah on 7/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
