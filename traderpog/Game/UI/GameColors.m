b491afae438c5f45a9f5b52cbb5f528ec5919be4
Shu Chiun Cheah <shunobi@gmail.com>
2012-09-09 09:17:51 -0700

    new bubble bg color

--- traderpog/Game/UI/GameColors.m
+++ traderpog/Game/UI/GameColors.m
@@ -60,9 +60,9 @@
 
 + (UIColor*) bubbleBgColorWithAlpha:(CGFloat)alpha
 {
-    UIColor* bgColor = [UIColor colorWithRed:114.0f/255.0f
-                                            green:179.0f/255.0f
-                                             blue:186.0f/255.0f
+    UIColor* bgColor = [UIColor colorWithRed:0.0f/255.0f
+                                            green:112.0f/255.0f
+                                             blue:185.0f/255.0f
                                             alpha:alpha];
     return bgColor;
 }
