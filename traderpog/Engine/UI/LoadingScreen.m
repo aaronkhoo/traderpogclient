19524ccfe0b1c130a82dd0fa0903fa52ba4557d6
Shu Chiun Cheah <shunobi@gmail.com>
2012-09-13 18:22:32 -0700

    loading screen work in progress

--- traderpog/Engine/UI/LoadingScreen.m
+++ traderpog/Engine/UI/LoadingScreen.m
@@ -7,9 +7,16 @@
 //
 
 #import "LoadingScreen.h"
+#import "CircleView.h"
+#import "GameColors.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface LoadingScreen ()
+{
+    CircleView* _circle;
+}
 - (void) initBackgroundColor;
+- (void) initCircle;
 @end
 
 @implementation LoadingScreen
@@ -22,7 +29,6 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) 
     {
-        // Custom initialization
     }
     return self;
 }
@@ -31,6 +37,7 @@
 {
     [super viewDidLoad];
     [self initBackgroundColor];
+    [self initCircle];
 }
 
 - (void)viewDidUnload
@@ -38,9 +45,8 @@
     _bigLabel = nil;
     _progressLabel = nil;
     _activityIndicator = nil;
+    _circle = nil;
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -54,5 +60,24 @@
     [self.view setBackgroundColor:[UIColor colorWithRed:237.0f/255.0f green:28.0f/255.0f blue:36.0f/255.0f alpha:1.0f]];
 }
 
+static const float kCircleRadiusInWidth = 0.25f;
+static const float kCircleVisibleFrac = 0.8f;
+static const float kCircleBorderFrac = 0.9f;
+static const float kCircleBorderWidth = 0.5f;
 
+- (void) initCircle
+{
+    float radius = kCircleRadiusInWidth * self.view.bounds.size.width;
+    float originX = (0.5f * self.view.bounds.size.width) - radius;
+    float originY = self.view.bounds.size.height - (kCircleVisibleFrac * radius * 2.0f);
+    CGRect circleFrame = CGRectMake(originX, originY, radius * 2.0f, radius * 2.0f);
+    _circle = [[CircleView alloc] initWithFrame:circleFrame
+                                     borderFrac:kCircleBorderFrac
+                                    borderWidth:kCircleBorderWidth
+                                    borderColor:[GameColors borderColorScanWithAlpha:1.0f]];
+    UIColor* circleColor = [GameColors bubbleColorScanWithAlpha:1.0f];
+    _circle.borderCircle.backgroundColor = circleColor;
+    _circle.coloredView.layer.shadowColor = circleColor.CGColor;
+    [self.view addSubview:_circle];
+}
 @end
