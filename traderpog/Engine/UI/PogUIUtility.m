f26d2bad9a8b89e3f23065e7ba256a5cbf8074e3
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-03 12:19:15 -0700

    wip push ngb2

--- traderpog/Engine/UI/PogUIUtility.m
+++ traderpog/Engine/UI/PogUIUtility.m
@@ -378,7 +378,7 @@ static const float kFadeAlertHeight = 40.0f;
 {
 #if DEBUG
     // if running from xcode, show "github" as version label
-    return @"ngb1";
+    return @"ngb2";
 #else
     // version string
     NSString *versionString = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString*)kCFBundleVersionKey];
