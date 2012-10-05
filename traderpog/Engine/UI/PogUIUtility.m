fbc7abd22d349650a206fbc2028876ad467493c8
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-05 10:58:07 -0700

    manually updated debug version to last commit SHA1

--- traderpog/Engine/UI/PogUIUtility.m
+++ traderpog/Engine/UI/PogUIUtility.m
@@ -385,7 +385,7 @@ static const float kFadeAlertHeight = 40.0f;
 {
 #if DEBUG
     // if running from xcode, show "github" as version label
-    return @"52d1ae2";
+    return @"3901604";
 #else
     // version string
     NSString *versionString = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString*)kCFBundleVersionKey];
