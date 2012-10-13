394e6ccda9a7b744d9ab31fa87deb8efcd93a2b4
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-13 13:11:59 -0700

    Added InfoViewController to project

---
+++ traderpog/Game/UI/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  traderpog
+//
+//  Created by Shu Chiun Cheah on 10/13/12.
+//  Copyright (c) 2012 GeoloPigs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
---
+++ traderpog/Game/UI/InfoViewController.m
@@ -0,0 +1,38 @@
+//
+//  InfoViewController.m
+//  traderpog
+//
+//  Created by Shu Chiun Cheah on 10/13/12.
+//  Copyright (c) 2012 GeoloPigs. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
