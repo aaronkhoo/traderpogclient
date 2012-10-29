16a84d83ba72051b68bd775f0b7cd6c8e0406560
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-29 16:08:45 -0700

    flyer_glider to 256x256
    * changed blimp to 256x256 as well; 128x128 is really blurry

---
+++ assets/game/flyer_glider/glider_res.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+sips -z 256 256 flyer_aerokit.png -o resized
+sips -z 256 256 flyer_aerokit_c1.png -o resized
+sips -z 256 256 flyer_aerokit_c2.png -o resized
+sips -z 256 256 flyer_aerokit_c3.png -o resized
+sips -z 256 256 flyer_aero.png -o resized
+sips -z 256 256 flyer_aero_c1.png -o resized
+sips -z 256 256 flyer_aero_c2.png -o resized
+sips -z 256 256 flyer_aero_c3.png -o resized
+sips -z 256 256 flyer_booster.png -o resized
+sips -z 256 256 flyer_booster_c1.png -o resized
+sips -z 256 256 flyer_booster_c2.png -o resized
+sips -z 256 256 flyer_booster_c3.png -o resized
+sips -z 256 256 flyer_boost.png -o resized
+sips -z 256 256 flyer_boost_c1.png -o resized
+sips -z 256 256 flyer_boost_c2.png -o resized
+sips -z 256 256 flyer_boost_c3.png -o resized
+sips -z 256 256 flyer_landed.png -o resized
+sips -z 256 256 flyer_landed_c1.png -o resized
+sips -z 256 256 flyer_landed_c2.png -o resized
+sips -z 256 256 flyer_landed_c3.png -o resized
+sips -z 256 256 flyer_blimp.png -o resized
+sips -z 256 256 flyer_c1.png -o resized
+sips -z 256 256 flyer_c2.png -o resized
+sips -z 256 256 flyer_c3.png -o resized
+
