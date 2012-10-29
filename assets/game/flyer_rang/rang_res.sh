9eeadf5d5a74f9ad7a38cadbbc1dcb8759636947
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-29 16:24:45 -0700

    wing and rang to 256x256

---
+++ assets/game/flyer_rang/rang_res.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+sips -z 256 256 flyer_rang_aerokit.png -o resized
+sips -z 256 256 flyer_rang_aerokit_c1.png -o resized
+sips -z 256 256 flyer_rang_aerokit_c2.png -o resized
+sips -z 256 256 flyer_rang_aerokit_c3.png -o resized
+sips -z 256 256 flyer_rang_aero.png -o resized
+sips -z 256 256 flyer_rang_aero_c1.png -o resized
+sips -z 256 256 flyer_rang_aero_c2.png -o resized
+sips -z 256 256 flyer_rang_aero_c3.png -o resized
+sips -z 256 256 flyer_rang_booster.png -o resized
+sips -z 256 256 flyer_rang_booster_c1.png -o resized
+sips -z 256 256 flyer_rang_booster_c2.png -o resized
+sips -z 256 256 flyer_rang_booster_c3.png -o resized
+sips -z 256 256 flyer_rang_boost.png -o resized
+sips -z 256 256 flyer_rang_boost_c1.png -o resized
+sips -z 256 256 flyer_rang_boost_c2.png -o resized
+sips -z 256 256 flyer_rang_boost_c3.png -o resized
+sips -z 256 256 flyer_rang_landed.png -o resized
+sips -z 256 256 flyer_rang_landed_c1.png -o resized
+sips -z 256 256 flyer_rang_landed_c2.png -o resized
+sips -z 256 256 flyer_rang_landed_c3.png -o resized
+sips -z 256 256 flyer_rang.png -o resized
+sips -z 256 256 flyer_rang_c1.png -o resized
+sips -z 256 256 flyer_rang_c2.png -o resized
+sips -z 256 256 flyer_rang_c3.png -o resized
+
---
+++ assets/game/flyer_wing/wing_res.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+sips -z 256 256 flyer_wing_aerokit.png -o resized
+sips -z 256 256 flyer_wing_aerokit_c1.png -o resized
+sips -z 256 256 flyer_wing_aerokit_c2.png -o resized
+sips -z 256 256 flyer_wing_aerokit_c3.png -o resized
+sips -z 256 256 flyer_wing_booster.png -o resized
+sips -z 256 256 flyer_wing_booster_c1.png -o resized
+sips -z 256 256 flyer_wing_booster_c2.png -o resized
+sips -z 256 256 flyer_wing_booster_c3.png -o resized
+sips -z 256 256 flyer_pwing_boost.png -o resized
+sips -z 256 256 flyer_pwing_boost_c1.png -o resized
+sips -z 256 256 flyer_pwing_boost_c2.png -o resized
+sips -z 256 256 flyer_pwing_boost_c3.png -o resized
+sips -z 256 256 flyer_wing_landed.png -o resized
+sips -z 256 256 flyer_wing_landed_c1.png -o resized
+sips -z 256 256 flyer_wing_landed_c2.png -o resized
+sips -z 256 256 flyer_wing_landed_c3.png -o resized
+sips -z 256 256 flyer_pwing.png -o resized
+sips -z 256 256 flyer_pwing_c1.png -o resized
+sips -z 256 256 flyer_pwing_c2.png -o resized
+sips -z 256 256 flyer_pwing_c3.png -o resized
+
