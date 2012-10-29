d613b004e4a84ccc4e45f2e7b31b087bf685a318
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-29 15:54:55 -0700

    reduced blimp pngs to 128x128

---
+++ assets/game/flyer_blimp/blimp_res.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+sips -z 128 128 flyer_blimp_aerokit.png -o resized
+sips -z 128 128 flyer_blimp_aerokit_c1.png -o resized
+sips -z 128 128 flyer_blimp_aerokit_c2.png -o resized
+sips -z 128 128 flyer_blimp_aerokit_c3.png -o resized
+sips -z 128 128 flyer_blimp_aero.png -o resized
+sips -z 128 128 flyer_blimp_aero_c1.png -o resized
+sips -z 128 128 flyer_blimp_aero_c2.png -o resized
+sips -z 128 128 flyer_blimp_aero_c3.png -o resized
+sips -z 128 128 flyer_blimp_booster.png -o resized
+sips -z 128 128 flyer_blimp_booster_c1.png -o resized
+sips -z 128 128 flyer_blimp_booster_c2.png -o resized
+sips -z 128 128 flyer_blimp_booster_c3.png -o resized
+sips -z 128 128 flyer_blimp_boost.png -o resized
+sips -z 128 128 flyer_blimp_boost_c1.png -o resized
+sips -z 128 128 flyer_blimp_boost_c2.png -o resized
+sips -z 128 128 flyer_blimp_boost_c3.png -o resized
+sips -z 128 128 flyer_blimp_landed.png -o resized
+sips -z 128 128 flyer_blimp_landed_c1.png -o resized
+sips -z 128 128 flyer_blimp_landed_c2.png -o resized
+sips -z 128 128 flyer_blimp_landed_c3.png -o resized
+sips -z 128 128 flyer_blimp.png -o resized
+sips -z 128 128 flyer_blimp_c1.png -o resized
+sips -z 128 128 flyer_blimp_c2.png -o resized
+sips -z 128 128 flyer_blimp_c3.png -o resized
+sips -z 128 128 icon_blimp.png -o resized
+sips -z 128 128 icon_blimp_c1.png -o resized
+sips -z 128 128 icon_blimp_c2.png -o resized
+sips -z 128 128 icon_blimp_c3.png -o resized
+
