74e5a5e6e0f814a6f2eca3b25da2cb52581e867b
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-30 08:16:18 -0700

    further downsized post annotations
    * and some unused post bubble images

---
+++ assets/Map/map_res.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sips -z 64 64 b_flyerlab.png -o resized
+sips -z 128 128 b_beacon.png -o resized
+sips -z 128 128 b_homebase.png -o resized
+sips -z 128 128 b_tradepost.png -o resized
+
---
+++ assets/UI/ui_res.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sips -z 64 64 bubble_postmark_g.png -o resized
+
