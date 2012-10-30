4e0291ea38e77d64f32e86783edbebf2759b886d
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-30 08:38:41 -0700

    icon_alerts to 64x64

---
+++ assets/anim/anim_res.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sips -z 64 64 icon_alert_flyer_001.png -o resized
+sips -z 64 64 icon_alert_flyer_002.png -o resized
+sips -z 64 64 icon_alert_flyer_003.png -o resized
+sips -z 64 64 icon_alert_post_001.png -o resized
+sips -z 64 64 icon_alert_post_002.png -o resized
+sips -z 64 64 icon_alert_post_003.png -o resized
+
