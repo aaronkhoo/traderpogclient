cd520563858a997e33f0ee60f4fa5b915befaffc
Shu Chiun Cheah <shunobi@gmail.com>
2012-10-30 08:44:49 -0700

    reduced pogresting to 128x128
    * also reduced the gray beacon bubble, only used when a player's fb
    image is not available

--- assets/UI/ui_res.sh
+++ assets/UI/ui_res.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 sips -z 64 64 bubble_postmark_g.png -o resized
+sips -z 128 128 bubble_beacon_fb.png -o resized
+sips -z 64 64 bubble_beacon_g_001.png -o resized
 
--- assets/anim/anim_res.sh
+++ assets/anim/anim_res.sh
@@ -6,4 +6,6 @@ sips -z 64 64 icon_alert_flyer_003.png -o resized
 sips -z 64 64 icon_alert_post_001.png -o resized
 sips -z 64 64 icon_alert_post_002.png -o resized
 sips -z 64 64 icon_alert_post_003.png -o resized
+sips -z 128 128 pogresting_001.png -o resized
+sips -z 128 128 pogresting_002.png -o resized
 
